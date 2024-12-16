c6f4defb57accddb20aa2cc8f701be1d47ba7555
hntramyy <hntramyy@gmail.com>
2024-12-16 19:53:33 +0700

    Initial commit

---
+++ src/pages/HuongDan.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const HuongDan: React.FC = () => {
+  return (
+    <div className="demo-container">
+      <div className="demo-link">
+        <b>MSV: B23DCCC116</b>{" "}
+        <b>Họ và tên: Hoàng Nguyễn Trà My </b>
+      </div>
+    </div>
+  );
+};
+
+export default HuongDan;
